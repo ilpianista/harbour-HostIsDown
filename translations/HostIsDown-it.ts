daee7b4761a07249cedf5a702c7c43b61737eb82
Andrea Scarpino <me@andreascarpino.it>
2014-09-09 22:57:59 +0200

    First release!

---
+++ translations/HostIsDown-it.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="37"/>
+        <source>Ping It!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="66"/>
+        <source>Ping any host!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/HostIsDown.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Ping It!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Ping any host!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
