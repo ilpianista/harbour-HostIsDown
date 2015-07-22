5bb383b2305620aa0e12e7047a698520187c5fe9
Andrea Scarpino <me@andreascarpino.it>
2015-07-23 00:05:56 +0200

    Add swedish translation and transifex config

---
+++ translations/harbour-hostisdown-sv.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="39"/>
+        <source>is UP!</source>
+        <translation>är UPPE!</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="43"/>
+        <source>is UNKNOWN!</source>
+        <translation>är OKÄND!</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="48"/>
+        <source>is DOWN!</source>
+        <translation>är NERE!</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="69"/>
+        <source>Ping It!</source>
+        <translation>Pinga den!</translation>
+    </message>
+</context>
+<context>
+    <name>HostDelegate</name>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="67"/>
+        <source>It was UP</source>
+        <translation>Den var UPPE</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="68"/>
+        <source>It was DOWN</source>
+        <translation>Den var NERE</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="70"/>
+        <source>It was UNKNOWN</source>
+        <translation>Den var OKÄND</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="76"/>
+        <source>Forget</source>
+        <translation>Glöm</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="87"/>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="46"/>
+        <source>The host is UP!</source>
+        <translation>Värddatorn är UPPE!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="50"/>
+        <source>The host is UNKNOWN!</source>
+        <translation>Värddatorn är OKÄND!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="55"/>
+        <source>The host is DOWN!</source>
+        <translation>Värddatorn är NERE!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="72"/>
+        <source>Ping all</source>
+        <translation>Pinga alla</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="81"/>
+        <source>Clear history</source>
+        <translation>Rensa historiken</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="95"/>
+        <source>Ping any host!</source>
+        <translation>Pinga en värddator!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="103"/>
+        <source>An IP address or hostname</source>
+        <translation>En IP-adress eller ett värdnamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="120"/>
+        <source>IPv6</source>
+        <translation>IPv6</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="136"/>
+        <source>Ping it</source>
+        <translation>Pinga den</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="147"/>
+        <source>History</source>
+        <translation>Historik</translation>
+    </message>
+</context>
+</TS>
