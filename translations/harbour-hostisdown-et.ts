5f2a1184a1b6ae081c1b008832bec5bdf1739743
Kristjan Räts <kristjanrats@gmail.com>
2018-02-11 10:45:09 +0000

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: Harbour HostIsDown/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-hostisdown/translations/et/

--- translations/harbour-hostisdown-et.ts
+++ translations/harbour-hostisdown-et.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="39"/>
         <source>is UP!</source>
-        <translation></translation>
+        <translation>on ÜLEVAL!</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="43"/>
         <source>is UNKNOWN!</source>
-        <translation></translation>
+        <translation>on TUNDMATU!</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="48"/>
         <source>is DOWN!</source>
-        <translation></translation>
+        <translation>on MAAS!</translation>
     </message>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="69"/>
         <source>Ping It!</source>
-        <translation></translation>
+        <translation>Pingi!</translation>
     </message>
 </context>
 <context>
@@ -29,32 +29,32 @@
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="67"/>
         <source>It was UP</source>
-        <translation></translation>
+        <translation>See oli ÜLEVAL</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="68"/>
         <source>It was DOWN</source>
-        <translation></translation>
+        <translation>See oli MAAS</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="70"/>
         <source>It was UNKNOWN</source>
-        <translation></translation>
+        <translation>See oli TUNDMATU</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="76"/>
         <source>Forget</source>
-        <translation></translation>
+        <translation>Unusta</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="81"/>
         <source>Open SSH</source>
-        <translation></translation>
+        <translation>Ava SSH</translation>
     </message>
     <message>
         <location filename="../qml/pages/HostDelegate.qml" line="92"/>
         <source>Deleting</source>
-        <translation></translation>
+        <translation>Kustutatakse</translation>
     </message>
 </context>
 <context>
@@ -62,57 +62,57 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="46"/>
         <source>The host is UP!</source>
-        <translation></translation>
+        <translation>Host on ÜLEVAL!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="50"/>
         <source>The host is UNKNOWN!</source>
-        <translation></translation>
+        <translation>Host on TUNDMATU!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="55"/>
         <source>The host is DOWN!</source>
-        <translation></translation>
+        <translation>Host on MAAS!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="66"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Seaded</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="73"/>
         <source>Ping all</source>
-        <translation></translation>
+        <translation>Pingi kõiki</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="86"/>
         <source>Clear history</source>
-        <translation></translation>
+        <translation>Tühjenda ajalugu</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="104"/>
         <source>Ping any host!</source>
-        <translation></translation>
+        <translation>Pingi mõnda hosti!</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="112"/>
         <source>An IP address or hostname</source>
-        <translation></translation>
+        <translation>IP aadress või hosti nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="133"/>
         <source>Use IPv6</source>
-        <translation></translation>
+        <translation>Kasuta IPv6&apos;t</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="149"/>
         <source>Ping it</source>
-        <translation></translation>
+        <translation>Pingi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="157"/>
         <source>History</source>
-        <translation></translation>
+        <translation>Ajalugu</translation>
     </message>
 </context>
 <context>
@@ -120,22 +120,22 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="36"/>
         <source>Settings</source>
-        <translation></translation>
+        <translation>Seaded</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="42"/>
         <source>Enable poller</source>
-        <translation></translation>
+        <translation>Luba pollija</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="43"/>
         <source>Pings every N minutes all hosts in the history</source>
-        <translation></translation>
+        <translation>Pingib ajaloost kõiki hoste iga N minuti tagant</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="51"/>
         <source>Poller interval in minutes</source>
-        <translation></translation>
+        <translation>Pollija intervall minutites</translation>
     </message>
 </context>
 </TS>
