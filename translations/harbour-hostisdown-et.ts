56e292a616c6ac376e013be29afc197ec43d8afe
Kristjan Räts <kristjanrats@gmail.com>
2018-02-11 10:29:40 +0000

    Added translation using Weblate (Estonian)

---
+++ translations/harbour-hostisdown-et.ts
@@ -0,0 +1,141 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="39"/>
+        <source>is UP!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="43"/>
+        <source>is UNKNOWN!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="48"/>
+        <source>is DOWN!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="69"/>
+        <source>Ping It!</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>HostDelegate</name>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="67"/>
+        <source>It was UP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="68"/>
+        <source>It was DOWN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="70"/>
+        <source>It was UNKNOWN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="76"/>
+        <source>Forget</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="81"/>
+        <source>Open SSH</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/HostDelegate.qml" line="92"/>
+        <source>Deleting</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="46"/>
+        <source>The host is UP!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="50"/>
+        <source>The host is UNKNOWN!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="55"/>
+        <source>The host is DOWN!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="66"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="73"/>
+        <source>Ping all</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="86"/>
+        <source>Clear history</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="104"/>
+        <source>Ping any host!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="112"/>
+        <source>An IP address or hostname</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="133"/>
+        <source>Use IPv6</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="149"/>
+        <source>Ping it</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="157"/>
+        <source>History</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="36"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="42"/>
+        <source>Enable poller</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="43"/>
+        <source>Pings every N minutes all hosts in the history</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="51"/>
+        <source>Poller interval in minutes</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
