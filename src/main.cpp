6fc367a89df8f78f3b260c1b0014f52479cede1d
Andrea Scarpino <andrea@scarpino.dev>
2021-06-05 19:46:22 +0200

    Fix copyrights

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
   The MIT License (MIT)
 
-  Copyright (c) 2014-2016-2021 Andrea Scarpino <andrea@scarpino.dev>
+  Copyright (c) 2014-2021 Andrea Scarpino <andrea@scarpino.dev>
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
